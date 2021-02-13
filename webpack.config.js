87cf3c67c3d723ee37a9acfebb9ed39e5188f68b
Kateryna Tytova <titova.kat.85@gmail.com>
2021-02-13 16:01:46 +0200

    init: start react-game task

---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        loader: "file-loader",
+        options: {
+          name: "[path][name].[ext]",
+          publicPath: "./",
+        },
+      },
+      {
+        test: /\.(woff2?|ttf|otf|eot)$/,
+        exclude: /node_modules/,
+        loader: "file-loader",
+        options: {
+          name: "[path][name].[ext]",
+        },
+      },
+      {
+        test: /\.json$/,
+        use: ["json-loader"],
+        type: "javascript/auto",
+      },
+      {
+        test: /\.(mp3|wav)$/,
+        use: [{
+          loader: "file-loader",
+          options: {
+            name: "sounds/[hash]-[name].[ext]",
+          },
+        }],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          context: path.resolve(__dirname, "src"),
+          from: "./assets",
+          to: "./dist/assets",
+        },
+      ],
+    }),
+  ],
+};
